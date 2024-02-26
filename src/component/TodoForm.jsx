9d752fc52a4e36115b7eadb15ebe833f657de09e
Yasin.Rabbani <www.yasinrabani1386@gmail.com>
2024-02-26 22:44:31 +0330

    first commit

--- src/component/TodoForm.jsx
+++ src/component/TodoForm.jsx
@@ -1,44 +1,102 @@
-import App from "../App"
+const TodoForm =()=> {
 
-const TodoForm  = () => {
-    const todo =[
-        {name: "task 1", privority: "1" , time:"8PM"},
-        {name: "task 2", privority: "2" , time:"8AM"},
-    ]
-    return(
-        <div style={{
-            display: "flex",
-            flexDirection:"column",
-            alignItems: "center",
-            justifyContent: "center",
-            width:"30%",
-            height:"30%",
-            padding:"5rem",
-            border: "3px dashed #ff9600",
-        }}>
-            <h3>Todo form</h3>
-            {todo.map((element) => (
-                <div
-                    key={element.name}
-                    style={{
-                        border: "2px solid black",
-                        display:"block",
-                        marginBottom:"10px",
-                    }}
-                >
-                    {element.name} {element.privority}
-                </div>
-            ))}
-            <input
-                type="text"
-                placeholder="Add New Ts"
-                style={{
-                    border:"2px solid black",
-                    display:"block",
-                }}
-            ></input>
-     </div>
-    )
-    
-}
-export default TodoForm
+  const todo = [
+      { name: "task 1", priority: "1" },
+  ]
+
+  function deleteText1() {
+      var element = document.getElementById("deletetext");
+      element.parentNode.removeChild(element);
+  }
+
+  function line(){
+   document.getElementById("LineText").style.textDecoration="line-through";
+   document.getElementById("LineText").style.color="red";
+  }
+
+  return(
+   <div
+    style={{
+        display: "flex",
+        justifyContent:"center",
+        alignItems: "center",
+        width:"100%",
+        height:"100%",
+        border:"7px dashed red",
+        minHeight:100,
+        borderRadius:20,
+        
+        flexDirection:"column",
+        background:"#FF9600",
+   }}
+   >
+      <h1
+            style={{ 
+              fontSize:"2rem",
+            }}>Todo List</h1>
+          {todo.map((element,index) => (
+              <div key={index}>
+                  {element.name} {element.priority}
+              </div>
+          ))}
+          <form>
+              <input type="text" name="n" defaultValue="Name:"
+              style={{
+                  display:"block",
+                  marginBottom:10,
+                  height:30,
+                  borderRadius:6,
+                  border:0,
+              }}
+              >
+              </input>
+              <input type="text" name="e" defaultValue="Email:"
+              style={{
+                  display:"block",
+                  marginBottom:5,
+                  borderRadius:6,
+                  height:30,
+                  border:0,
+              }}
+              >
+              </input>
+              </form>
+              
+              <button 
+              style={{
+                  background:"green",
+                  marginBottom:5,
+                  width:100,
+              }}
+              >Send</button>
+                      <h1 id="LineText"
+            style={{
+                color:"white",
+            }}
+            >Add.</h1>
+          <button onClick={line} 
+            style={{
+                width:100,
+                background:"blue",
+                color:"white",
+                fontSize:20,
+                marginBottom:20,
+           }}
+          >Add</button>
+
+              <h1 id="deletetext"
+              style={{
+                  color:"red",
+              }}
+              >Delete  text</h1>
+          <button onClick={deleteText1} 
+              style={{
+                width:100,
+                background:"red",
+            }}
+          >Delete</button>
+    </div>
+  );
+};
+
+export default TodoForm;
